3ddaf4f923b8f77d969e4581c8a2371dbac0ae4c
Edmond Doan <edmonddoan@gmail.com>
2025-06-21 21:45:30 -0500

    add chart

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+import numpy as np
 
 ticker = 'AAPL'
 start_date = '2020-01-01'
@@ -22,3 +23,18 @@ signals['long_mavg'] = data['Close'].rolling(window=long_window, min_periods=1, 
 
 print(signals.head())
 
+signals['signal'] = np.where(signals['short_mavg'] > signals['long_mavg'], 1.0, 0.0)
+
+signals['positions'] = signals['signal'].diff()
+
+print(signals[signals['positions'] !=0].head())
+
+fig = plt.figure(figsize=(12, 8))
+ax1 = fig.add_subplot(111, ylabel=f'{ticker} Price in $')
+
+signals['price'].plot(ax=ax1, color='gray', lw=1.)
+signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
+
+plt.title('Simple Moving Average Strategy')
+plt.legend()
+plt.show()
